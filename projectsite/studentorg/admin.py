3df7bcdeec8e8a10f58d6f773c9afb50fa425bce
Laxusimfinite11 <laxustaladro@gmail.com>
2024-10-28 11:16:08 +0800

    add new attributes Students, Organizations, Programs

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -2,13 +2,29 @@ from django.contrib import admin
 from .models import College, Program, Organization, Student, OrgMember
 
 admin.site.register(College)
-admin.site.register(Program)
-admin.site.register(Organization)
+
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ("prog_name", "college")
+    search_fields = ("name",)
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("name", "college")
+    search_fields = ("name",)
 
 @admin.register(Student)
 class OrgMemberAdmin(admin.ModelAdmin):
-    list_display = ("student_id", "lastname", "firstname", "middlename", "program")
+    list_display = ("student_id", "lastname", "firstname", "middlename", "program", "get_college")
     search_fields = ("lastname", "firstname",)
+
+    def get_college(self, obj):
+        try:
+            prog = Program.objects.get(prog_name=obj.program)
+            return prog.college
+        
+        except Program.DoesNotExist:
+            return None
 
 @admin.register(OrgMember)
 class OrgMemberrAdmin(admin.ModelAdmin):
